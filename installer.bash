71339a269334e9bbff5fd89fc7e906473ba240a2
MaskedTrench <markushasorokin@yandex.ru>
2022-03-07 01:27:33 +0300

    Updates

--- installer.bash
+++ installer.bash
@@ -1,7 +1,4 @@
-pip install virtualenv
-virtualenv venv
-source venv/bin/activate
-pip install -r requirements.txt
-export FLASK_ENV=development
-export FLASK_APP=run.py
-flask run
+pip install -r requirements.txt;
+export FLASK_ENV=development;
+export FLASK_APP=run.py;
+flask run;
