0c7a1b2cdb461adb311d509f21eed853da07c04e
IamVirgoo <90650865+IamVirgoo@users.noreply.github.com>
2022-03-07 00:38:21 +0300

    Ababa

--- app/views.py
+++ app/views.py
@@ -39,38 +39,77 @@ def page_not_found(e):
 class Price(Resource):
     def get(self, price):
         print(f"Getted price: {price}" )
-        return {'content': Price_Calculation(price)}
+        return {'content': "ababa"}
 
 
 class Prices(Resource):
     def get(self, key):
         print(f"Getting {key}")
-        return {'content': get_by_key(key)}
+        return {'content': priceCalc(key)}
 
 api.add_resource(Price, '/api/v1/<string:price>')
 api.add_resource(Prices, '/api/v2/<string:key>')
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------
 
-def DollarParse():
-    url = "https://ru.investing.com/currencies/usd-rub"
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    usd = soup.find('span', class_='text-2xl').text
-    return usd
+def priceCalc(key):
 
-with open('app/message.json', 'r') as databasefile:
-    data = json.load(databasefile)['cont']
+    # Create the dollar parser
 
-def get_by_key(key: str):
+    def DollarParse():
+        url = "https://ru.investing.com/currencies/usd-rub"
+        headers = {'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_23 like Mac OS X) '
+                                 'AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'}
+
+        r = requests.get(url, headers=headers)
+        soup = BeautifulSoup(r.content, 'html.parser')
+
+        usd = soup.find('span', class_='text-2xl').text
+        return usd
+
+    # Open the json file with data
+
+    with codecs.open('message.json', 'r', 'utf-8') as databasefile:
+        data = json.load(databasefile)['cont']
+
+    # Declaration of variables
+
+    averagePrice = 0
+    counter = 0
+    dollarStat = [60.9579, 67.0349, 58.3529, 62.7091, 64.7362, 72.1464, 73.6541]
+    dollarActual = float(DollarParse().replace(',', '.'))
+
+    # Running through the database
+
     for i in data:
-        print(i['Наименование'])
-        k = i['Наименование'].split(' ')[2]
-        print(k)
+        k = i['Наименование']
         if k == key:
-            return {
-                'Name': i['Наименование'],
-                'DelivDate': i['ДатаПоставки'],
-                'Price': i['Цена'],
-                'Amount': i['ОбъемЗаказа']
-            }
+            counter += 1
+            if i['ДатаПоставки'][:4] == "2015" or i['ДатаПоставки'][6:10] == "2015":
+                averagePrice += float(i['Цена'].replace(',', '.')) / dollarStat[0]
+
+            if i['ДатаПоставки'][:4] == "2016" or i['ДатаПоставки'][6:10] == "2016":
+                averagePrice += float(i['Цена'].replace(',', '.')) / dollarStat[1]
+
+            if i['ДатаПоставки'][:4] == "2017" or i['ДатаПоставки'][6:10] == "2017":
+                averagePrice += float(i['Цена'].replace(',', '.')) / dollarStat[2]
+
+            if i['ДатаПоставки'][:4] == "2018" or i['ДатаПоставки'][6:10] == "2018":
+                averagePrice += float(i['Цена'].replace(',', '.')) / dollarStat[3]
+
+            if i['ДатаПоставки'][:4] == "2019" or i['ДатаПоставки'][6:10] == "2019":
+                averagePrice += float(i['Цена'].replace(',', '.')) / dollarStat[4]
+
+            if i['ДатаПоставки'][:4] == "2020" or i['ДатаПоставки'][6:10] == "2020":
+                averagePrice += float(i['Цена'].replace(',', '.')) / dollarStat[5]
+
+            if i['ДатаПоставки'][:4] == "2021" or i['ДатаПоставки'][6:10] == "2021":
+                averagePrice += float(i['Цена'].replace(',', '.')) / dollarStat[6]
+
+            if i['ДатаПоставки'][:4] == "2022" or i['ДатаПоставки'][6:10] == "2022":
+                averagePrice += float(i['Цена'].replace(',', '.')) / dollarActual
+
+    # Output
+
+    averagePrice /= counter
+    return averagePrice
