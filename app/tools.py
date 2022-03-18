63fcc94794a793574db992bebc0fcb2d61350a6b
YHT97 <91371115+YHT97@users.noreply.github.com>
2022-03-19 01:51:31 +0300

    ver_5

--- app/tools.py
+++ app/tools.py
@@ -20,21 +20,19 @@ def get_data(url: str) -> dict:
 
 
 # TODO: override this function
-def price_dollar() -> str:
-    url = "https://ru.investing.com/currencies/usd-rub"
-    r = re.get(url)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    usd = soup.find('span', class_='text-2xl').text
-    return usd
+urleur = "https://ru.investing.com/currencies/eur-rub"
+url_dollar = "https://ru.investing.com/currencies/usd-rub"
+r = re.get(url_dollar)
+r = re.get(urleur)
+usd = BeautifulSoup(r.content, 'html.parser').find('span', class_='text-2xl').text
+eur = BeautifulSoup(r.content, 'html.parser').find('span', class_='text-2xl').text 
 
-
+dollarActual = float(usd.replace(',', '.'))
+euroActual = float(eur.replace(',', '.'))
 # TODO: override this function
-def price_euro() -> str:
-    url = "https://ru.investing.com/currencies/eur-rub"
-    r = re.get(url)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    eur = soup.find('span', class_='text-2xl').text
-    return eur    
+
+
+
 
 
 # TODO: override this function
@@ -46,56 +44,64 @@ def priceCalc(key):
     quintity = 0
     dollarStat = [60.9579, 67.0349, 58.3529, 62.7091, 64.7362, 72.1464, 73.6541]
     euroStat = [87.1877, 82.4488, 72.5021, 73.9511, 65.9014, 74.231, 67.7767]
-
-    dollarActual = float(price_dollar().replace(',', '.'))
-    euroActual = float(price_euro().replace(',', '.'))
+    lastname=''
     
     # Running through the database
     for i in data:
         k = i['Наименование']
+        """ if k!=lastname:
+            continue """
         if k == key:
-            print(f'Finded key is {key}')
+           # lastname=k
+           # print(f'Finded key is {key}')
             counter += 1
-            if i['ДатаПоставки'][:4] == "2015" or i['ДатаПоставки'][6:10] == "2015":
-                averageDollarPrice += float(i['Цена'].replace(',', '.')) / dollarStat[0]
-                averageEuroPrise += float(i['Цена'].replace(',', '.')) / euroStat[0]
-                quintity += int(i["ОбъемЗаказа"])
+            if i['Дата поставки'][:4] == "2015" or i['Дата поставки'][6:10] == "2015":
+                averageDollarPrice += float(i['Цена, руб'].replace(',', '.')) / dollarStat[0]
+                averageEuroPrise += float(i['Цена, руб'].replace(',', '.')) / euroStat[0]
+                quintity += int(i["Объем заказа"])
 
 
-            if i['ДатаПоставки'][:4] == "2016" or i['ДатаПоставки'][6:10] == "2016":
-                averageDollarPrice += float(i['Цена'].replace(',', '.')) / dollarStat[1]
-                averageEuroPrise += float(i['Цена'].replace(',', '.')) / euroStat[1]
-                quintity += int(i["ОбъемЗаказа"])
+            if i['Дата поставки'][:4] == "2016" or i['Дата поставки'][6:10] == "2016":
+                averageDollarPrice += float(i['Цена, руб'].replace(',', '.')) / dollarStat[1]
+                averageEuroPrise += float(i['Цена, руб'].replace(',', '.')) / euroStat[1]
+                quintity += int(i["Объем заказа"])
 
-            if i['ДатаПоставки'][:4] == "2017" or i['ДатаПоставки'][6:10] == "2017":
-                averageDollarPrice += float(i['Цена'].replace(',', '.')) / dollarStat[2]
-                averageEuroPrise += float(i['Цена'].replace(',', '.')) / euroStat[2]
-                quintity += int(i["ОбъемЗаказа"])
 
-            if i['ДатаПоставки'][:4] == "2018" or i['ДатаПоставки'][6:10] == "2018":
-                averageDollarPrice += float(i['Цена'].replace(',', '.')) / dollarStat[3]
-                averageEuroPrise += float(i['Цена'].replace(',', '.')) / euroStat[3]
-                quintity += int(i["ОбъемЗаказа"])
+            if i['Дата поставки'][:4] == "2017" or i['Дата поставки'][6:10] == "2017":
+                averageDollarPrice += float(i['Цена, руб'].replace(',', '.')) / dollarStat[2]
+                averageEuroPrise += float(i['Цена, руб'].replace(',', '.')) / euroStat[2]
+                quintity += int(i["Объем заказа"])
 
-            if i['ДатаПоставки'][:4] == "2019" or i['ДатаПоставки'][6:10] == "2019":
-                averageDollarPrice += float(i['Цена'].replace(',', '.')) / dollarStat[4]
-                averageEuroPrise += float(i['Цена'].replace(',', '.')) / euroStat[4]
-                quintity += int(i["ОбъемЗаказа"])
 
-            if i['ДатаПоставки'][:4] == "2020" or i['ДатаПоставки'][6:10] == "2020":
-                averageDollarPrice += float(i['Цена'].replace(',', '.')) / dollarStat[5]
-                averageEuroPrise += float(i['Цена'].replace(',', '.')) / euroStat[5]
-                quintity += int(i["ОбъемЗаказа"])
+            if i['Дата поставки'][:4] == "2018" or i['Дата поставки'][6:10] == "2018":
+                averageDollarPrice += float(i['Цена, руб'].replace(',', '.')) / dollarStat[3]
+                averageEuroPrise += float(i['Цена, руб'].replace(',', '.')) / euroStat[3]
+                quintity += int(i["Объем заказа"])
 
-            if i['ДатаПоставки'][:4] == "2021" or i['ДатаПоставки'][6:10] == "2021":
-                averageDollarPrice += float(i['Цена'].replace(',', '.')) / dollarStat[6]
-                averageEuroPrise += float(i['Цена'].replace(',', '.')) / euroStat[6]
-                quintity += int(i["ОбъемЗаказа"])
 
-            if i['ДатаПоставки'][:4] == "2022" or i['ДатаПоставки'][6:10] == "2022":
-                averageDollarPrice += float(i['Цена'].replace(',', '.')) / dollarActual
-                averageEuroPrise += float(i['Цена'].replace(',', '.')) / euroStat
-                quintity += int(i["ОбъемЗаказа"])
+            if i['Дата поставки'][:4] == "2019" or i['Дата поставки'][6:10] == "2019":
+                averageDollarPrice += float(i['Цена, руб'].replace(',', '.')) / dollarStat[4]
+                averageEuroPrise += float(i['Цена, руб'].replace(',', '.')) / euroStat[4]
+                quintity += int(i["Объем заказа"])
+
+
+            if i['Дата поставки'][:4] == "2020" or i['Дата поставки'][6:10] == "2020":
+                averageDollarPrice += float(i['Цена, руб'].replace(',', '.')) / dollarStat[5]
+                averageEuroPrise += float(i['Цена, руб'].replace(',', '.')) / euroStat[5]
+                quintity += int(i["Объем заказа"])
+
+
+            if i['Дата поставки'][:4] == "2021" or i['Дата поставки'][6:10] == "2021":
+                averageDollarPrice += float(i['Цена, руб'].replace(',', '.')) / dollarStat[6]
+                averageEuroPrise += float(i['Цена, руб'].replace(',', '.')) / euroStat[6]
+                quintity += int(i["Объем заказа"])
+
+
+            if i['Дата поставки'][:4] == "2022" or i['Дата поставки'][6:10] == "2022":
+                averageDollarPrice += float(i['Цена, руб'].replace(',', '.')) / dollarActual
+                averageEuroPrise += float(i['Цена, руб'].replace(',', '.')) / euroStat
+                quintity += int(i["Объем заказа"])
+
 
     # Output
     if counter == 0:
@@ -103,9 +109,16 @@ def priceCalc(key):
     averageDollarPrice /= counter
     averageEuroPrise/=counter
     quintity /= counter
-    averagePrice = (averageDollarPrice*dollarActual + averageEuroPrise*euroActual)/2
-    return averagePrice, quintity
+    averagePriceRUB = (averageDollarPrice*dollarActual + averageEuroPrise*euroActual)/2
+    return averagePriceRUB, quintity
 
+def Array_Init():
+    top_list = ['Вал 3519.05.02.007','Вал 3519.05.02.083','Вал 3532.10.01.011','Вал 3536.11.01.002','Вал 3572.05.10.150','Вал 3572.05.10.200','ВАЛ-ШЕСТЕРНЯ 3536.11.01.002' ,'Вант стрелы 00.1606.49.1 ','Засов 3532.01.02.007','Зуб  1085.52.05-1','Зуб 1085.52.05-1','Колесо 3519.05.02.003','Колесо 3519.05.02.006','Колесо 3519.05.02.061','Коромысло 3519.21.00.023','Коромысло 3537.25.00.500','Ось 3519.05.02.081','Ось 3519.05.02.082','Ось 3519.21.00.025','Ось 3536.03.00.001','Сателлит 3536.11.01.028']
+    a=[usd,eur]
+    for i in top_list:
+        a.append(i)
+        a.append(priceCalc(i))
+    return a
 
 if __name__ == "__main__":
     res = get_data(url="https://docs.google.com/spreadsheets/d/e/2PACX-1vR5TcnFfw2KNSB0QXgVBmuIpZRad_mcD7XRtVH0zITO1Etzvjfs4Tf2L5aArkOovQ/pub?output=csv")
