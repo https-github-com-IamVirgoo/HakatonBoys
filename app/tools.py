352f2aa1e2e664d2670969a0f3e2cc8965669b5f
YHT97 <91371115+YHT97@users.noreply.github.com>
2022-03-19 17:35:53 +0300

    ver_6

--- app/tools.py
+++ app/tools.py
@@ -22,8 +22,8 @@ def get_data(url: str) -> dict:
 # TODO: override this function
 urleur = "https://ru.investing.com/currencies/eur-rub"
 url_dollar = "https://ru.investing.com/currencies/usd-rub"
-r_usd = re.get(url_dollar)
-r_eur = re.get(urleur)
+r_usd = requests.get(url_dollar)
+r_eur = requests.get(urleur)
 usd = BeautifulSoup(r_usd.content, 'html.parser').find('span', class_='text-2xl').text
 eur = BeautifulSoup(r_eur.content, 'html.parser').find('span', class_='text-2xl').text 
 
@@ -116,6 +116,8 @@ def priceCalc(key):
 
 top_list = ['Вал 3519.05.02.007','Вал 3519.05.02.083','Вал 3532.10.01.011','Вал 3536.11.01.002','Вал 3572.05.10.150','Вал 3572.05.10.200','ВАЛ-ШЕСТЕРНЯ 3536.11.01.002' ,'Вант стрелы 00.1606.49.1 ','Засов 3532.01.02.007','Зуб  1085.52.05-1','Зуб 1085.52.05-1','Колесо 3519.05.02.003','Колесо 3519.05.02.006','Колесо 3519.05.02.061','Коромысло 3519.21.00.023','Коромысло 3537.25.00.500','Ось 3519.05.02.081','Ось 3519.05.02.082','Ось 3519.21.00.025','Ось 3536.03.00.001','Сателлит 3536.11.01.028']
 a=[]
+data=json.dump(get_data(url="https://docs.google.com/spreadsheets/d/e/2PACX-1vR5TcnFfw2KNSB0QXgVBmuIpZRad_mcD7XRtVH0zITO1Etzvjfs4Tf2L5aArkOovQ/pub?output=csv"))
+
 for i in top_list:
     a.append(i)
     a.append(priceCalc(i))
